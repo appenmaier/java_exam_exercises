13c84d76945ae9f0d6c0990b4beea9f0335a7938
appenmaier <47243617+appenmaier@users.noreply.github.com>
2024-03-15 09:36:31 +0100

    Add Cashier System Solution (#3)
    * Create cashiersystem
    
    * ✨ added solution for cashiersystem exercise
    
    * 🧑‍💻 added missing @Override to the toString()-methods
    
    * Format Code, Add Documentation Comments and Fix Output
    
    ---------
    
    Co-authored-by: Anarchiebald <152083055+Anarchiebald@users.noreply.github.com>
    Co-authored-by: Anarchiebald <bornholdt@stud.dhbw-ravensburg.de>

---
+++ exams1/cashiersystem/ClassOfGoods.java
@@ -0,0 +1,25 @@
+package exams1.cashiersystem;
+
+/**
+ * Warengruppe
+ *
+ * @author Anarchiebald
+ * @version 1.0
+ *
+ */
+public enum ClassOfGoods {
+
+  BAKERY_PRODUCTS("Backwaren"), DAIRY_PRODUCTS("Milchprodukte"), FRUITS("Obst"),
+  VEGETABLES("Gemuese");
+
+  private final String description;
+
+  ClassOfGoods(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+}
---
+++ exams1/cashiersystem/Goods.java
@@ -0,0 +1,30 @@
+package exams1.cashiersystem;
+
+/**
+ * Ware
+ *
+ * @author Anarchiebald
+ * @version 1.0
+ *
+ */
+public class Goods extends Product {
+
+  private final ClassOfGoods classOfGoods;
+
+  public Goods(int id, String description, String unit, double priceInEuro,
+      ClassOfGoods classOfGoods) {
+    super(id, description, unit, priceInEuro);
+    this.classOfGoods = classOfGoods;
+  }
+
+  public ClassOfGoods classOfGoods() {
+    return classOfGoods;
+  }
+
+  @Override
+  public String toString() {
+    return "Goods [id=" + id() + ", description=" + description() + ", unit=" + getUnit()
+        + ", priceInEuro=" + getPriceInEuro() + ", classOfGoods=" + classOfGoods + "]";
+  }
+
+}
