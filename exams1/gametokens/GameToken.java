cdc67ab4213900ddd18335b2704f67089726af33
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-07-29 15:35:37 +0200

    Add GameTokens

---
+++ exams1/gametokens/GameToken.java
@@ -0,0 +1,61 @@
+package exams1.gametokens;
+
+import java.util.Objects;
+
+/**
+ * Spielfigur
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class GameToken {
+
+  private final GameTokenType type;
+  private final GameTokenColor color;
+  private final int value;
+
+  public GameToken(GameTokenType type, GameTokenColor color, int value) {
+    this.type = type;
+    this.color = color;
+    this.value = value;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    GameToken other = (GameToken) obj;
+    return color == other.color && type == other.type && value == other.value;
+  }
+
+  public GameTokenColor getColor() {
+    return color;
+  }
+
+  public GameTokenType getType() {
+    return type;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(color, type, value);
+  }
+
+  @Override
+  public String toString() {
+    return "GameToken [type=" + type + ", color=" + color + ", value=" + value + "]";
+  }
+
+}
---
+++ exams1/gametokens/GameTokenColor.java
@@ -0,0 +1,14 @@
+package exams1.gametokens;
+
+/**
+ * Spielfigurfarbe
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public enum GameTokenColor {
+
+  RED, BLUE, GREEN, WHITE, BLACK;
+
+}
