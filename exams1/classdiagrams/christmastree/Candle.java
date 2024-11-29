41e225ecf6adbc138436bdbbfca1f040685638ed
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-11-29 09:57:17 +0100

    Typo

--- exams1/classdiagrams/christmastree/Candle.java
+++ exams1/classdiagrams/christmastree/Candle.java
@@ -24,12 +24,15 @@ public class Candle {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null) {
       return false;
-    if (getClass() != obj.getClass())
+    }
+    if (getClass() != obj.getClass()) {
       return false;
+    }
     Candle other = (Candle) obj;
     return Objects.equals(colour, other.colour) && isBurning == other.isBurning;
   }
@@ -39,7 +42,7 @@ public class Candle {
     return Objects.hash(colour, isBurning);
   }
 
-  public boolean isBuring() {
+  public boolean isBurning() {
     return isBurning;
   }
 
